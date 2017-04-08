1bd53560d372768f20703a928580341b23da183d
Z-liu <liuzhao@yeah.net>
2017-04-08 22:42:14 +0800

    20170408

--- app/main/errors.py
+++ app/main/errors.py
@@ -3,3 +3,13 @@ Created on 2017Äê4ÔÂ5ÈÕ
 
 @author: Administrator
 '''
+from flask import render_template
+from . import main
+
+@main.app_errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@main.app_errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
--- manage.py
+++ manage.py
@@ -3,3 +3,16 @@ Created on 2017Äê4ÔÂ5ÈÕ
 
 @author: Administrator
 '''
+import os
+from app import create_app,db
+from app.models import User,Role
+from flask_script import Manager,Shell
+from flask_migrate import Migrate,MigrateCommand
+
+app=create_app(os.getenv('FLASK_CONFIG'))
+manager=Manager(app)
+migrate=Migrate(app,db)
+
+def make_shell_context():
+    return dict(app=app,db=db,User=User,Role=Role)
+manager.add_command('shell',Shell(make_context=make_shell_context))
